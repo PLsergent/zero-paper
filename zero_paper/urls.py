6fddafebda210ebc10394d5ea667160fa4198c55
PL Sergent <papa.louis59@gmail.com>
2021-07-06 17:42:26 +0200

    feature/init_template_and_controller (#4)

---
+++ zero_paper/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from django.conf import settings
+
+from . import views
+
+urlpatterns = [
+    path('', views.homepage, name="homepage"),
+    path('user/', include('django.contrib.auth.urls'))
+]
